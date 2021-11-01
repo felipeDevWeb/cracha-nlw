e7c344b192edfa3b724b6a50ae6d04ef25119a6a
Felipe Gabriel <78567673+felipeDevWeb@users.noreply.github.com>
2021-11-01 19:02:24 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const userLinks = {
+  github: 'felipeDevWeb',
+  //youtube: '',
+  facebook: 'felipe.gabriel.73113528',
+  instagram: 'felipegb.silva'/*,
+  twitter: ''
+  */
+}
+
+function changeUserLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${userLinks[social]}`
+  }
+}
+changeUserLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${userLinks.github}`
+  fetch(url).then(response => response.json()).then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userImage.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+}
+
+getGithubProfileInfos()
